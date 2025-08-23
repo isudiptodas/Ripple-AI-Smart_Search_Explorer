1a209d61520d97fa27d4bc1bf79452c34b68b25b
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-08-24 04:40:36 +0530

    Added supabase admin client

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,5 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseServiceRole = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRole);
